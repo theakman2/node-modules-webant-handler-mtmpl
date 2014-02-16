8943ca493474eb23f1332b166106829d42c86588
theakman2 <theakman2@hotmail.com>
2014-02-16 17:36:30 +0000

    Updating to accommodate webant v4 API changes.

---
+++ test/index.js
@@ -0,0 +1,67 @@
+var fs = require("fs");
+var path = require("path");
+var vm = require("vm");
+var childProcess = require("child_process");
+
+var shellEscape = require("shell-escape");
+var webant = require("webant");
+
+var handler = require("../lib/index.js");
+
+function phantom(assert,done,cb) {
+	var pjs = childProcess.exec(
+		'phantomjs ' + shellEscape([path.join(__dirname,"headless","phantomwebant.js")]),
+		{
+			cwd:path.join(__dirname,"headless"),
+			maxBuffer:1024*1024
+		},
+		function(err,stdout,stderr) {
+			pjs.kill();
+			if (err) {
+				assert.fail("phantomjs reports an error: " + err);
+				done();
+				return;
+			}
+			if (stderr) {
+				assert.fail("phantomjs reports content in stderror: " + stderr);
+				done();
+				return;
+			}
+			var out;
+			try {
+				out = JSON.parse(stdout.trim());
+			} catch(e) {
+				assert.fail("Could not JSON.parse() stdout [stdout is: " + stdout + "]");
+				done();
+				return;
+			}
+			cb(out);
+		}
+	);
+}
+
+var tests = {
+	"test handler":function(assert,done) {
+		webant({
+			entry:path.join(__dirname,"headless","entry.js"),
+			dest:path.join(__dirname,"headless","main.js"),
+			handlers:[handler]
+		},function(err){
+			if (err) {
+				assert.fail("Webant should not error when parsing javascript (error: " + err + ")");
+				done();
+				return;
+			}
+			phantom(assert,done,function(out){
+				assert.strictEqual(
+					out,
+					"My name is Mario!",
+					"mtmpl should be compiled correctly"
+				);
+				done();
+			});
+		});
+	}
+};
+
+require("test").run(tests);
